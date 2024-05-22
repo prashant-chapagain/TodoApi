888f23d8603765a94ba0e01ac44cfa3bdbb38c07
Prashant <prashantchapagain@gmail.com>
2024-05-22 21:45:15 +0545

    Used MapGroup method

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,18 +6,20 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList "
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app=builder.Build();
 
-app.MapGet("/todoitems", async (TodoDb db) =>
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/todoitems", async (TodoDb db) =>
 await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete",async (TodoDb db)=>
+todoItems.MapGet("/todoitems/complete",async (TodoDb db)=>
 await db.Todos.Where(t=>t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
 await db.Todos.FindAsync(id)
     is Todo todo
     ? Results.Ok(todo)
     : Results.NotFound());
-app.MapPost("/todoitems", async (Todo todo,TodoDb db) =>
+todoItems.MapPost("/todoitems", async (Todo todo,TodoDb db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
@@ -26,7 +28,7 @@ app.MapPost("/todoitems", async (Todo todo,TodoDb db) =>
 }
 
 );
-app.MapPut("/todoitems/{id}", async (int id,Todo updatedTodo, TodoDb db) => {
+todoItems.MapPut("/todoitems/{id}", async (int id,Todo updatedTodo, TodoDb db) => {
     var todo = await db.Todos.FindAsync(id);
     if (todo is null) return Results.NotFound();
 
@@ -39,7 +41,7 @@ app.MapPut("/todoitems/{id}", async (int id,Todo updatedTodo, TodoDb db) => {
 
     }
 );
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
     if(await db.Todos.FindAsync(id) is Todo todo)
     {
